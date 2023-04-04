d1f5d72f445ec1cf57c5c325362956c9c1181247
atulhari <atulhari3011@gmail.com>
2023-04-05 00:20:37 +0100

    Image undistortion params

--- VslamOne/src/imageUndistortion.cpp
+++ VslamOne/src/imageUndistortion.cpp
@@ -2,3 +2,17 @@
 #include <string>
 
 string image_file "";
+int main(int argc, char **argv)
+{       
+    assert(argv[1]!=NULL);
+
+    // Specify a rad-tan model 
+    double k1 = 7, p1=14, p2=27;
+    double fx=250, fy=270, cx=286, cy=299;
+    cv::Mat image = cv::imread(argv[1],0);
+    double rows = image.rows, cols = image.cols;
+    cv::Mat undistorted_image = cv::Mat(rows,cols,CV_8U);
+
+    return 0;
+
+}
